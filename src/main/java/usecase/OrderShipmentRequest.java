dde76e1e979236019a3a658d337b9bddab34cf4b
Tamara Jordan <tamara.jordan@springernature.com>
2024-02-13 09:07:05 +0000

    Simplify package names

---
+++ src/main/java/usecase/OrderShipmentRequest.java
@@ -0,0 +1,13 @@
+package usecase;
+
+public class OrderShipmentRequest {
+    private int orderId;
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+}
---
+++ src/main/java/usecase/SellItemsRequest.java
@@ -0,0 +1,15 @@
+package usecase;
+
+import java.util.List;
+
+public class SellItemsRequest {
+    private List<SellItemRequest> requests;
+
+    public void setRequests(List<SellItemRequest> requests) {
+        this.requests = requests;
+    }
+
+    public List<SellItemRequest> getRequests() {
+        return requests;
+    }
+}
